b59b45b1ce108e892034b12956f6a8050f648977
Cavin <cstone2003@hotmail.com>
2014-03-08 10:22:26 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kevinstone::Application.config.secret_token = '4b5940056a913bed91e32ef75aa9690b7e2cec9c99a6a066e9f53296b7c94ea9b356c8eb575059d85f593e8e5118218176230406e66ff17ba61805f5527936f9'
