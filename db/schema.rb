e9225efae341407c9302ca2b2529717649c4f8a2
Cavin <cstone2003@hotmail.com>
2014-03-08 18:13:51 -0600

    Add videos scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140308233845) do
+ActiveRecord::Schema.define(:version => 20140309001234) do
 
   create_table "dynamic_contents", :force => true do |t|
     t.string   "key"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(:version => 20140308233845) do
     t.string   "photo_image"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "videos", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "source"
+    t.boolean  "allow_comments"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
 end
