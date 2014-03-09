5553b40c4ce820d5d93aea74a4d18bbea82e3117
Cavin <cstone2003@hotmail.com>
2014-03-09 13:03:36 -0500

    Add :allow_comments to attr_accessible

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :description, :photo_image, :title, :remove_photo_image
+  attr_accessible :description, :photo_image, :title, :remove_photo_image, :allow_comments
   mount_uploader :photo_image, PhotosImageUploader
 end
