b9b2c76e443f5a475cd65667e913bdf645ecad01
Cavin <cstone2003@hotmail.com>
2014-03-09 16:21:32 -0500

    Add thumbnail to blog images

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ActiveRecord::Base
   attr_accessible :article_image, :body, :title, :remove_article_image
 
   mount_uploader :article_image, ArticleImageUploader
+
+  default_scope order('created_at DESC')
 end
