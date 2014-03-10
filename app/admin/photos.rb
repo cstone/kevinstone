9417648e8bb44057dd48f2c1cd5c24eb13552365
Cavin <cstone2003@hotmail.com>
2014-03-10 11:23:40 -0500

    Update Photos Index page for ActiveAdmin

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Photo do
+
+  index do
+    selectable_column
+    column :photo_image do |image|
+      image_tag(image.photo_image.url(:thumb))
+    end
+    column :title
+    column :allow_comments
+    default_actions
+  end
   
 end
