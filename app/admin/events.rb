5a5553a6f7bb4950e9fed427e32046ac6056dd04
Cavin <cstone2003@hotmail.com>
2014-03-10 11:48:40 -0500

    Update Events Show page for ActiveAdmin

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -5,5 +5,26 @@ ActiveAdmin.register Event do
       column :title
       column :time
       default_actions
+  end
+
+
+  show do |event|
+    attributes_table do
+      row :id
+      row :title
+      row :description do
+      simple_format event.description
     end
+      row :venue
+      row :address
+      row :city
+      row :state
+      row :zip
+      row :url
+      row :time
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
 end
