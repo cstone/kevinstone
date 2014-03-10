e94a1912744ac47ddff33bb8703aa9506d69da3a
Cavin <cstone2003@hotmail.com>
2014-03-10 11:41:26 -0500

    Update Articles Show page for ActiveAdmin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -10,10 +10,24 @@ ActiveAdmin.register Article do
     default_actions
   end
 
-
-
   filter :title
   filter :body
   filter :allow_comments
+
+
+  show do |article|
+    attributes_table do
+      row :id
+      row :title
+      row :body do
+        simple_format article.body
+      end
+      row :allow_comments
+      row :article_image do
+        image_tag(article.article_image.url(:thumb))
+      end
+    end
+    active_admin_comments
+  end
 
 end
