880b4a22a6628aab8752f9d338a36f1563cfa6a7
Cavin <cstone2003@hotmail.com>
2014-03-10 11:55:05 -0500

    Update Photos Show page for ActiveAdmin

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -9,5 +9,24 @@ ActiveAdmin.register Photo do
     column :allow_comments
     default_actions
   end
+
+
+
+  show do |photo|
+    attributes_table do
+      row :id
+      row :title
+      row :description do
+        simple_format photo.description
+      end
+      row :photo_image do
+        image_tag(photo.photo_image.url(:thumb))
+      end
+      row :allow_comments
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
   
 end
