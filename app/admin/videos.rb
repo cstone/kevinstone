9a762b07027380a3939cc4ab45962c354759a6a9
Cavin <cstone2003@hotmail.com>
2014-03-10 11:51:06 -0500

    Update Videos Show page for ActiveAdmin

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -7,5 +7,22 @@ ActiveAdmin.register Video do
     column :allow_comments
     default_actions
   end
+
+
+
+  show do |video|
+    attributes_table do
+      row :id
+      row :title
+      row :description do
+        simple_format video.description
+      end
+      row :source
+      row :allow_comments
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
   
 end
