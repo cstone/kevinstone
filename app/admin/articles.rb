5b2169468476f1993fc2e270996877bdf3ea97a1
Cavin <cstone2003@hotmail.com>
2014-03-10 10:14:48 -0500

    Update Articles Index page in ActiveAdmin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Article do
-  
+
+  index do
+    column :title
+    column :article_image do |image|
+      image_tag(image.article_image.url)
+    end
+    column :allow_comments
+    default_actions
+  end
+
+  filter :title
+  filter :body
+  filter :allow_comments
+
 end
--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,8 @@
 ActiveAdmin.register Event do
-  
+
+  index do
+      column :title
+      column :time
+      default_actions
+    end
 end
