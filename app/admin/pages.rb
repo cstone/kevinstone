9f16c683492255a95b0fbae3692b587df4efe478
Cavin <cstone2003@hotmail.com>
2014-03-10 17:11:38 -0500

    Add find_by_permalink to Pages in ActiveAdmin

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,4 +1,7 @@
 ActiveAdmin.register Page do
+  controller do
+    defaults :finder => :find_by_permalink
+  end
 
   index do
     selectable_column
@@ -8,5 +11,23 @@ ActiveAdmin.register Page do
     column :name
     column :permalink
     default_actions
+  end
+
+
+  show do |page|
+    attributes_table do
+      row :id
+      row :name
+      row :permalink
+      row :content do
+        simple_format page.content
+      end
+      row :page_image do
+        image_tag(page.page_image.url(:thumb))
+      end
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
   end
 end
