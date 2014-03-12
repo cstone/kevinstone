17ab24f6945e18e2b8fecc9811029e4d4cc4057c
Cavin <cstone2003@hotmail.com>
2014-03-11 19:46:10 -0500

    Add active_admin_editor

---
+++ config/initializers/active_admin_editor.rb
@@ -0,0 +1,6 @@
+ActiveAdmin::Editor.configure do |config|
+   config.s3_bucket = 'kevinjstone'
+   config.aws_access_key_id = 'AKIAI45WULOXZXAGOUJA'
+   config.aws_access_secret = '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'
+   config.storage_dir = 'uploads'
+end
