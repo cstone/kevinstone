ac644ce1084e0b7184af49e0d87929728463b161
Cavin <cstone2003@hotmail.com>
2014-03-14 22:52:30 -0500

    Remove images from active admin for articles and pages

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -20,9 +20,7 @@ ActiveAdmin.register Article do
 
   index do
     selectable_column
-    column :article_image do |image|
-      image_tag(image.article_image.url(:thumb))
-    end
+    column :article_image
     column :title
     column :allow_comments
     default_actions
--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -5,9 +5,7 @@ ActiveAdmin.register Page do
 
   index do
     selectable_column
-    column :page_image do |image|
-      image_tag(image.page_image.url(:thumb))
-    end
+    column :page_image
     column :name
     column :permalink
     default_actions
