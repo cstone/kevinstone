100a986512912075968395a7c7b4316d67939e7f
Cavin <cstone2003@hotmail.com>
2014-03-31 17:27:21 -0500

    Add datetime_picker

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ActiveRecord::Base
   attr_accessible :address, :city, :description, :state, :time, :title, :url, :venue, :zip
 
+  just_define_datetime_picker :time, :add_to_attr_accessible => true
+  validates :time, :presence => true
+
 
   scope :upcoming, where("time >= ?", Time.now).order("time")
   scope :next_three, where("time >= ?", Time.now).order("time").limit(3)
