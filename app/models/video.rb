310dd0dcd02b9a7b17f54cd6a60ed590e3193ac8
Cavin <cstone2003@hotmail.com>
2014-04-01 13:23:31 -0500

    Add video validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,7 @@
 class Video < ActiveRecord::Base
   attr_accessible :allow_comments, :description, :source, :title
 
+  validates_presence_of :title, :description, :source
+
   default_scope order('created_at DESC')
 end
