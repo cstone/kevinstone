fda66d00c97b4071a501f4958e7529d49727460c
Cavin <cstone2003@hotmail.com>
2014-03-31 20:33:50 -0500

    Remove image from pages index in activeadmin

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -5,9 +5,6 @@ ActiveAdmin.register Page do
 
   index do
     selectable_column
-    column :page_image do |image|
-      image_tag(image.page_image.url(:thumb))
-    end
     column :name
     column :permalink
     default_actions
