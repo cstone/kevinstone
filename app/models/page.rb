e85bed90a70f943d0e460ca6f98610655d784a2a
Cavin <cstone2003@hotmail.com>
2014-04-01 13:22:32 -0500

    Add page validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,7 @@ class Page < ActiveRecord::Base
   attr_accessible :content, :name, :permalink, :page_image, :remove_page_image
   mount_uploader :page_image, PageImageUploader
   validates_uniqueness_of :permalink
+  validates_presence_of :name, :permalink, :content
 
   def to_param
     permalink
