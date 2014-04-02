e18f1d93f0b72bad3a158a52db280a365513fefa
Cavin <cstone2003@hotmail.com>
2014-04-01 22:19:54 -0500

    Remove ActiveAdmin Dashboard content

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,17 +2,15 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  #content :title => proc{ I18n.t("active_admin.dashboard") } do
-  #  div :class => "blank_slate_container", :id => "dashboard_default_message" do
-  #    span :class => "blank_slate" do
-  #      span I18n.t("active_admin.dashboard_welcome.welcome")
-  #      small I18n.t("active_admin.dashboard_welcome.call_to_action")
-  #    end
-  #  end
-  #end # content
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    #div :class => "blank_slate_container", :id => "dashboard_default_message" do
+    #  span :class => "blank_slate" do
+    #    span I18n.t("active_admin.dashboard_welcome.welcome")
+    #    small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #  end
+    #end
 
-
-  columns do
+    columns do
       column do
         panel "Recent Articles" do
           ul do
@@ -49,4 +47,5 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
+  end # content
 end
