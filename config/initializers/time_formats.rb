91ca075cc145344e3cf5f605467ffe048f17ce87
Cavin <cstone2003@hotmail.com>
2014-04-02 13:17:03 -0500

    Update :my_format to include date

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -6,4 +6,4 @@ Time::DATE_FORMATS[:date_time] = '%m/%d/%Y %I:%M %P'
 Time::DATE_FORMATS[:date_only] = '%m/%d/%Y'
 
 # config/initializers/date_and_time_formats.rb
-Time::DATE_FORMATS[:my_format] = '%I:%M %P' # 12:30pm
+Time::DATE_FORMATS[:my_format] = '%m/%d/%Y %I:%M %P' # 12:30pm
