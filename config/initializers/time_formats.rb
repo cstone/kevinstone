7305ab891db43b2c5411b0e4dbacbcd7e05f5db4
Cavin <cstone2003@hotmail.com>
2014-04-02 12:03:31 -0500

    Add :date_only to time_formats.rb

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,6 @@
-Time::DATE_FORMATS[:default] = "%m/%d/%Y %I:%M %P"
+Time::DATE_FORMATS[:default] = '%m/%d/%Y %I:%M %P'
+
+Time::DATE_FORMATS[:date_only] = '%m/%d/%Y'
 
 # config/initializers/date_and_time_formats.rb
 Time::DATE_FORMATS[:my_format] = '%I:%M %P' # 12:30pm
